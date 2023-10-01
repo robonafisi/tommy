503021ba7b973b669f2b02743212fe6daeafc57a
Mohammad Nafisi <nafisidev@gmail.com>
2023-10-01 01:26:57 -0700

    Here

--- page5.py
+++ page5.py
@@ -20,14 +20,14 @@ root.attributes("-fullscreen", True)
 
 logo = tk.PhotoImage(file="Tommy.png")  # Replace 'logo.png' with your file
 logo_label = tk.Label(root, image=logo, bg='pink')
-logo_label.place(x=900, y=50)  # Adjust the x and y coordinates as needed
+logo_label.place(x=1200, y=250)  # Adjust the x and y coordinates as needed
 
 # Add the 'sub_heading' label at the top left
 sub_heading = tk.Label(root, text="Michael, Congratulations!!", font=('calibri', 80, 'bold'), bg='pink', justify=tk.LEFT, wraplength=1850)
 sub_heading.place(x=10, y=10)
 
 # Display text indicating the number of tablets taken
-tablets_taken = tk.Label(root, text="You have taken 4 tablets of Melatonin!\nWell done!!", font=('calibri', 60, 'bold'), bg='pink', justify=tk.LEFT, wraplength=1850)
+tablets_taken = tk.Label(root, text="You have taken 4 tablets of Melatonin!\nWell done!!", font=('calibri', 60, 'bold'), bg='pink', justify=tk.LEFT, wraplength=1200)
 tablets_taken.place(x=10, y=250)
 
 root.mainloop()
